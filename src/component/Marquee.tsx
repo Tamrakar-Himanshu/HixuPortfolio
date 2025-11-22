1493ecf578d542f00cefedcac200559fb289aec6
Himanshu Tamrakar <himanshutamrakar64742@gmail.com>
2025-11-22 18:48:51 +0530

    minor bug fix

--- src/component/Marquee.tsx
+++ src/component/Marquee.tsx
@@ -136,7 +136,7 @@ const Marquee = ({
 
     const observer = Observer.create({
       onChangeY(self) {
-        let factor = 5.5;
+        let factor = 2.5;
         if ((!reverse && self.deltaY < 0) || (reverse && self.deltaY > 0)) {
           factor *= -1;
         }
